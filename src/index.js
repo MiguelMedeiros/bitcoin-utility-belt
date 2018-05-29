edbc27dfa1e5d36bc6729bf31e402400815453e2
MiguelMedeiros\Miguel Medeiros <miguel.medeiros@gmail.com>
2018-05-29 13:31:00 -0300

    linting, comments and variables names
    linting, comments and variables names

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var wif = require("wif");
 var bigi = require("bigi");
 var safeBuffer = require("safe-buffer").Buffer;
 
-var createWalvar = function (type = "default", testnet = false){
+var createWallet = function (type = "default", testnet = false){
   try{
     // check network: bitcoin or testnet
     var network;
@@ -31,15 +31,14 @@ var createWalvar = function (type = "default", testnet = false){
         };
         return wallet;
       }
-    }
-    catch(err){
-      console.log(err);
-      return false;
-    }
-  };
+  } catch(err){
+    console.log(err);
+    return false;
+  }
+};
 
-  var createBrainWalvar = function(passphrase, testnet = false){
-   try{
+var createBrainWallet = function(passphrase, testnet = false){
+  try{
     // check network: bitcoin or testnet
     var network;
     if(testnet){
@@ -49,21 +48,21 @@ var createWalvar = function (type = "default", testnet = false){
     }
 
     // create a hass buffer from passphrase
-    var hashBuffer = bitcoin.crypto.sha256(bitcoin.crypto.sha256(safeBuffer.from(passphrase)));
-   var d = bigi.fromBuffer(hashBuffer);
+    var passphraseHash = bitcoin.crypto.sha256(safeBuffer.from(passphrase));
+    var hashBuffer = bitcoin.crypto.sha256(passphraseHash);
+    var d = bigi.fromBuffer(hashBuffer);
 
-      // create an eliptic curve
-      var keyPair = new bitcoin.ECPair(d, null, {network: network});
+    // create an eliptic curve
+    var keyPair = new bitcoin.ECPair(d, null, {network: network});
 
-      // return wallet
-      var wallet;
-      wallet = {
+    // return wallet with address and private key
+    var wallet;
+    wallet = {
        "address": keyPair.getAddress(),
        "privateKey": keyPair.toWIF()
-     };
-     return wallet;
-   }
-   catch(err){
+    };
+    return wallet;
+  } catch(err){
     console.log(err);
     return false;
   }
@@ -78,10 +77,13 @@ var getAddress = function(privateKey, testnet = false){
     }else{
       network = bitcoin.networks.bitcoin;
     }
+
+    // get eliptic curves from wif
     var keyPair = bitcoin.ECPair.fromWIF(privateKey, network);
+
+    // return address
     return keyPair.getAddress();
-  }
-  catch(err){
+  } catch(err){
     console.log(err);
     return false;
   }
@@ -99,6 +101,8 @@ var validateAddress = function(address, testnet = false){
 
     // try to output script to validate address
     bitcoin.address.toOutputScript(address, network);
+
+    // no errors return true
     return true;
   } catch (err) {
     //console.log(err);
@@ -125,11 +129,12 @@ var signMessage = function(message, privateKey, testnet = false){
 
     // get private key buffer
     var privateKeyBuffer = keyPair.d.toBuffer(32);
+    var compressed = keyPair.compressed;
 
     // return signature
-    return bitcoinMessage.sign(message, privateKeyBuffer, keyPair.compressed).toString("base64");
-  }
-  catch(err){
+    var signature = bitcoinMessage.sign(message, privateKeyBuffer, compressed);
+    return signature.toString("base64");
+  } catch(err){
     console.log(err);
     return false;
   }
@@ -145,63 +150,75 @@ var verifyMessage = function (message, address, signature, testnet = false){
       network = bitcoin.networks.bitcoin;
     }
 
+    // validate address
     if(validateAddress(address, network)){
       throw new Error("Invalid address");
     }
     if(signature === "" && signature.lenght !== 64){
       throw new Error("Invalid signature");
     }
+
+    // return true or false after varify message
     return bitcoinMessage.verify(message, address, signature);
-  }
-  catch(err){
+  } catch(err){
     console.log(err);
     return false;
   }
 };
 
-var bip38Encrypt = function(privateKey, passphrase){
+var bip38Encrypt = function(privateKey, passphrase){  
   try{
+    // validate private key and passphrase
     if(privateKey === ""){
       throw new Error("Invalid private key");
     }
     if(passphrase === ""){
       throw new Error("Invalid passphrase");
     }
-   var decoded = wif.decode(privateKey);
-   var encryptedKey = bip38.encrypt(decoded.privateKey, decoded.compressed, passphrase);
 
-   return encryptedKey;
- }
- catch(err){
-  console.log(err);
-  return false;
-}
+    // encrypt key
+    var decoded = wif.decode(privateKey);
+    var decodedPK = decoded.privateKey;
+    var compressed = decoded.compressed;
+    var encryptedKey = bip38.encrypt(decodedPK, compressed, passphrase);
+
+    // return encrypted key
+    return encryptedKey;
+  } catch(err){
+    console.log(err);
+    return false;
+  }
 };
 
 var bip38Decrypt = function (encryptedKey, passphrase, consoleLog = false){
   try {
+    // validate encrypte key and passphrase
     if(encryptedKey === ""){
       throw new Error("Invalid encrypted key");
     }
     if(passphrase === ""){
       throw new Error("Invalid passphrase");
     }
-    var decryptedKey = bip38.decrypt(encryptedKey, passphrase, function (status) {
+
+    // decrypt key
+    var decryptedKey = bip38.decrypt(encryptedKey,passphrase,function(status){
       if(consoleLog){
         console.log((status.percent).toFixed(2));
       }
     });
+
+    // return wif private key
     return wif.encode(0x80, decryptedKey.privateKey, decryptedKey.compressed);
-  }
-  catch(err){
+  } catch(err){
     console.log(err);
     return false;
   }
 };
 
+// export functions
 module.exports = {
-  createWalvar: createWalvar,
-  createBrainWalvar: createBrainWalvar,
+  createWallet: createWallet,
+  createBrainWallet: createBrainWallet,
   getAddress: getAddress,
   validateAddress: validateAddress,
   signMessage: signMessage,
